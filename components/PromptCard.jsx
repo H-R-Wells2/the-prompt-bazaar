0eec4f441f28b2de7763e78869cb4946ac5a8946
h-r-wells2 <hrwells10246@gmail.com>
2023-07-22 01:55:27 +0530

    creator bug fixed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
+import { toast } from 'react-toastify';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession;
@@ -13,24 +14,48 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
   const handleProfileClick = () => {
-
     if (post.creator._id === session?.user.id) return router.push("/profile");
 
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
 
+  const [creator, setCreator] = useState({
+    username: "deleted account",
+    email: "NA",
+    image: "/assets/images/logo.svg",
+  });
+
+  useEffect(() => {
+    if (post.creator) {
+      setCreator(post.creator);
+    }
+  }, [post.creator]);
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
+    toast.success("Copied to clipboard", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
     setTimeout(() => setCopied(""), 3000);
   };
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
+        <div
+          className="flex-1 flex-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
-            src={post.creator.image}
+            src={creator.image}
             alt="User Profile"
             width={40}
             height={40}
@@ -38,10 +63,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {creator.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {creator.email}
             </p>
           </div>
         </div>
@@ -68,7 +93,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         #{post.tag}
       </button>
 
-      {session?.user.id === post.creator.id && pathName === "/profile" && (
+      {session?.user.id === creator.id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-200 pt-3">
           <button
             className="font-inter text-sm green_gradient cursor-pointer"
